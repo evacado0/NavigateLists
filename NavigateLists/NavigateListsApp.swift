455b0108a27ad0cdb7b3b1c0b851f7a86760ea0c
evacado14 <68162501+evacado14@users.noreply.github.com>
2023-08-11 10:53:56 -0400

    Initial Commit

---
+++ NavigateLists/NavigateListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigateListsApp.swift
+//  NavigateLists
+//
+//  Created by Eva Skarabot on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigateListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
